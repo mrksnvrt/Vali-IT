d7db5c21880238560caa192c3eadf9031fef9137
Marek Siinvert <mareksiinvert@gmail.com>
2021-04-14 09:42:33 +0300

    .

--- src/main/java/ee/bcs/valiit/tasks/Lesson2b.java
+++ src/main/java/ee/bcs/valiit/tasks/Lesson2b.java
@@ -1,44 +1,87 @@
 package ee.bcs.valiit.tasks;
 
+import java.util.Arrays;
+
 public class Lesson2b {
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(exercise1(5)));
+        System.out.println(Arrays.toString(sampleArray()));
+        System.out.println(Arrays.toString(generateArray(5)));
+        System.out.println(Arrays.toString(decreasingArray(-4)));
+        System.out.println(Arrays.toString(yl3(5)));
+
+
+
     }
-    // TODO trüki välja n arvu
+    //******************************************************************************************************************
+    // TODO EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1EXERCISE1
+    //trüki välja n arvu
     // näiteks
     // sisend: 5
     // trüki välja: 1 2 3 4 5
-    public static void exercise1(int n){
-
+    public static int[] exercise1(int n){
+        int [] nArvuValja = new int[n];
+        for (int i=0; i < n; i++){
+            nArvuValja[i] = i + 1;
+        }
+        return nArvuValja;
     }
-
-    // TODO tagasta massiiv milles oleks numbrid 1,2,3,4,5
-    public int[] sampleArray(){
-        return new int[0];
+    //********************************************************************************************************************
+    // TODO sampleArraysampleArraysampleArraysampleArraysampleArraysampleArraysampleArraysampleArraysampleArraysampleArray
+    //tagasta massiiv milles oleks numbrid 1,2,3,4,5
+    public static int[] sampleArray(){
+        int[] viisArvuValja = new int [5];
+        for (int i=0; i < viisArvuValja.length; i++){
+            viisArvuValja[i] = i+1;
+        }
+        return viisArvuValja;
     }
-
-    // TODO loo massiiv mis saab sisendiks n ja tagastab massiivi suurusega n
+    //***************************************************************************************************************************
+    // TODO generateArraygenerateArraygenerateArraygenerateArraygenerateArraygenerateArraygenerateArraygenerateArraygenerateArray
+    //loo massiiv mis saab sisendiks n ja tagastab massiivi suurusega n
     // Kus esimene element on 1 ja iga järnev arv on 1 võrra suurem
     // näiteks:
     // sisend: 5
     // vastus: {1, 2, 3, 4, 5}
-    public static int[] generateArray(int n){
-        return new int[0];
-    }
 
-    // TODO
+    public static int[] generateArray(int n){
+        int [] genereerinMassiivi = new int [n];
+        for (int i=0; i < n; i++){
+            genereerinMassiivi[i] = i+1;
+        }
+        return genereerinMassiivi;
+    }
+    //*****************************************************************************************************************************
+    // TODO decreasingArraydecreasingArraydecreasingArraydecreasingArraydecreasingArraydecreasingArraydecreasingArraydecreasingArray
     // Tagastada massiiv kus oleks numbrid n-ist 0 ini
     // Näiteks: sisend: 5
     // Väljund: 5, 4, 3, 2, 1, 0
     // Näide2: siend: -3
     // Väljund: -3, -2, -1, 0
     public static int[] decreasingArray(int n){
-        return new int[0];
+        if(n>0) {
+            int[] massiiv = new int [n+1];
+            for(int i=0; i < n; i++){
+            massiiv[i] = n-i;
+        }
+        return massiiv;
+        } else {
+            int[] massiiv = new int [-n+1];
+            for(int i=0; i<-n; i++){
+                massiiv[i] = n+i;
+            }
+            return massiiv;
+        }
     }
-
-    // TODO
+    //*************************************************************************************************************************
+    // TODO yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3yl3
     // tagasta massiiv pikkusega n, mille kõigi elementide väärtused on 3
     public static int[] yl3(int n){
-        return new int[0];
+        int [] miksKoikKolmed =  new int [n];
+        for(int i=0; i < n; i++){
+            miksKoikKolmed[i]=3;
+        }
+        return miksKoikKolmed;
     }
 }
--- src/main/java/ee/bcs/valiit/tests/Test1.java
+++ src/main/java/ee/bcs/valiit/tests/Test1.java
@@ -1,13 +1,33 @@
 package ee.bcs.valiit.tests;
 
+import java.util.Arrays;
+
 public class Test1 {
     public static void main(String[] args) {
+
+        System.out.println(onSelgevoiEiOleSelge(3));
+        System.out.println(onSelgevoiEiOleSelge(21));
+        int testArray[] = {6, 2, 7, 3, 1};
+        //System.out.println(Arrays.toString(addToArray(testArray,2));
+        System.out.println(Arrays.toString(addToArray(new int []{3,4,5},3)));
+        //System.out.println(Arrays.toString(addToArray({},4)));
     }
-//    ÜL 1
-//    Tee funktsioon, mis tagastab boolean väärtuse ja võtab sisse ühe parameetri
-//    funktsioon peab tagastama
-//		true: kui sisend parameeter jagub 3 või 7 (aga ei jagu mõlema numbriga)
-//            false: kui sisend parameeter ei jagu 3 ega 7 või jagub mõlema numbriga
+//ÜL 1
+//Tee funktsioon, mis tagastab boolean väärtuse ja võtab sisse ühe parameetri
+//funktsioon peab tagastama
+//true: kui sisend parameeter jagub 3 või 7 (aga ei jagu mõlema numbriga)
+//false: kui sisend parameeter ei jagu 3 ega 7 või jagub mõlema numbriga
+
+    public static boolean onSelgevoiEiOleSelge(int kasSeePeaksArvOlemaV){
+        boolean jagubVoiEiJaguSellesOnKüsimus = true;
+        if(kasSeePeaksArvOlemaV % 3 == 0 || kasSeePeaksArvOlemaV % 7 == 0) {
+            if(kasSeePeaksArvOlemaV % 3 == 0 && kasSeePeaksArvOlemaV % 7 == 0){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
 
 
     // ÜL2
@@ -16,7 +36,17 @@ public class Test1 {
     // sisend [1,2,3], 5
     // vastus [6,7,8]
     public static int[] addToArray(int[] array, int x){
-        return new int[0];
+        int arrayLenght = array.length;
+        for(int i = 0; i < arrayLenght; i++){
+            array[i] = array[i]+x;
+        }
+        return array;
     }
 
+    public static int[] funktsioon(int[]votabSisse){
+        //tagastab esimese väärtuse, mis on 0
+
+        votabSisse[0] = 0;
+        return votabSisse;
+    }
 }
